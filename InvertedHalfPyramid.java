3ce8505ac9ec605badc129dd6077dc804fda3644
shivrajgn7 <shivrajgn7@.com>
2025-08-12 18:50:59 +0530

    commiting pattren file

---
+++ InvertedHalfPyramid.java
@@ -0,0 +1,16 @@
+public class InvertedHalfPyramid {
+        public static void main(String[] args) {
+            int n=4;
+
+            for(int i=n;i>=1;i--){
+                for(int j=0;j<=i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+
+
+        }
+    }
+
